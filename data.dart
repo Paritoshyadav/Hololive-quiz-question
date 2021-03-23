37e773a2ba7ba7d5c9575ef372e1af56dbc62796
Paritoshyadav <y.paritosh@gmail.com>
2021-03-24 03:42:07 +0530

    Add files via upload

---
+++ data.dart
@@ -0,0 +1,293 @@
+Map ques = {
+  "response_code": 0,
+  "results": [
+    {
+      "category": "hololive",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What is yagoo real name?",
+      "correct_answer": "Motoaki Tanigo",
+      "incorrect_answers": ["Yagoo is Yagoo", "Motoaki seki", "Kazuyuki Fukuda"]
+    },
+    {
+      "category": "hololive",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "As of 2021, how many active Vtuber are manages by Hololive?",
+      "correct_answer": "52",
+      "incorrect_answers": ["46", "50", "56"]
+    },
+    {
+      "category": "hololive",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "Who comes with the name yagoo?",
+      "correct_answer": "subaru",
+      "incorrect_answers": ["Pekora", "Marine", "Haachama"]
+    },
+    {
+      "category": "hololive",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "Akai Haato is also known as",
+      "correct_answer": "Haachama",
+      "incorrect_answers": ["Pekora", "subaru", "Amane Kanata"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "On which date our smol shark hits 1m subscriber?",
+      "correct_answer": "22 Oct",
+      "incorrect_answers": ["5 Nov", "25 Dec", "26 Nov"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Who Quotes this " "You can't be mad at me... I'm cute!" " ",
+      "correct_answer": "gura",
+      "incorrect_answers": ["Anya", "Amelia", "Kiara"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Gura comes from _______?",
+      "correct_answer": "Atlantis",
+      "incorrect_answers": ["Sea", "Pond", "Underworld"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "what kind of shark  gawr gura is according to her?",
+      "correct_answer": "gura shark",
+      "incorrect_answers": ["salmon shark", "smol shark", "baby shark"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What is height of gura in cm?",
+      "correct_answer": "141",
+      "incorrect_answers": ["140", "145", "138"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What is name of gura mascot?",
+      "correct_answer": "Bloop",
+      "incorrect_answers": ["emergency food", "Chumbuds", "NOAA"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Are you a _____?",
+      "correct_answer": "Shrimp",
+      "incorrect_answers": ["simp"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Gura birthdate?(anniversary of her arrival on land)",
+      "correct_answer": "20 june",
+      "incorrect_answers": ["20 july", "25 august", "25 december"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Is gura is Shortest member of hololive",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What gura biggest fear?",
+      "correct_answer": "loud stomak",
+      "incorrect_answers": [
+        "hot sand",
+        "Swiming without floaties",
+        "eating bloop"
+      ]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What is gura age",
+      "correct_answer": "It's over 9,000!",
+      "incorrect_answers": ["9,361", "9,927", "9,492"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Which of the memeber is hydrodynamic",
+      "correct_answer": "gura",
+      "incorrect_answers": ["watson", "kiara", "Korone"]
+    },
+    {
+      "category": "gura",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Its ____",
+      "correct_answer": "a!",
+      "incorrect_answers": ["ahh!", "A!", "aa!"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What generation is Usada Pekora ~peko",
+      "correct_answer": "third",
+      "incorrect_answers": ["first", "second", "fifth"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "which option represent Pekora laugh ~peko",
+      "correct_answer": "AH↓ HA↑ HA↑ HA↑",
+      "incorrect_answers": ["hahaha", "HA↑ HA↑ HA↑", "lol"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "when pekora debuted on YouTube with her introductory stream",
+      "correct_answer": "17 July",
+      "incorrect_answers": ["28 May", "7 July ", "13 June"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Pekora is _______ memeber of hololive to reach 1 million YouTube subscribers",
+      "correct_answer": "fourth",
+      "incorrect_answers": ["second", "third", "Sixth"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Pekora's scarf is actually her pet named _____",
+      "correct_answer": "Don-chan",
+      "incorrect_answers": ["Nousagi", "Usada-chan", "cho-chan"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Usada Kensetsu (Pekora company on the hololive Minecraft JP server) first employee was ______.",
+      "correct_answer": "Moona Hoshinova",
+      "incorrect_answers": ["Amane Kanata", "Uruha Rushia", "Shirogane Noel"]
+    },
+    {
+      "category": "Pekora",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "according to Pekora, bunny girls don't wear panties",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Korone",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "If Human is turtle, Turtle is human,Turtle is korone,Korone is human,Human is doggo,Doggo is ______",
+      "correct_answer": "human",
+      "incorrect_answers": ["korone", "doggo", "turtle"]
+    },
+    {
+      "category": "Korone",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Korone favorite demon in doom, hint :Bethesda-san even give her stuff toy of that demon",
+      "correct_answer": "Cacodemon",
+      "incorrect_answers": ["Malwrath", "Wretched", "Pinky"]
+    },
+    {
+      "category": "Korone",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Which game reference Korone in their easter egg",
+      "correct_answer": "Doom Eternal",
+      "incorrect_answers": [
+        "Crash Bandicoot 4",
+        "Hidden Folks",
+        "Banjo-kazooie"
+      ]
+    },
+    {
+      "category": "Korone",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Inugami Korone is the _______ hololive VTuber to reach 1 million subscribers",
+      "correct_answer": "second",
+      "incorrect_answers": ["fourth", "third", "Sixth"]
+    },
+    {
+      "category": "Marine",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Marine reached 1,000,000 subscribers on YouTube, making her the _____ hololive member and the _____ member of the Japanese branch",
+      "correct_answer": "6th,5th",
+      "incorrect_answers": ["5th,3rd", "8th,5th", "7th,4th"]
+    },
+    {
+      "category": "Marine",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Marine allergic to",
+      "correct_answer": "Cats",
+      "incorrect_answers": ["Dogs", "Fish", "salt"]
+    },
+    {
+      "category": "Amelia",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Did Amelia reach 1m subscribers?,making her the 9th hololive member",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Amelia",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Name of Amelia mascot is",
+      "correct_answer": "Bubba",
+      "incorrect_answers": ["Bobbo", "doggo", "tea"]
+    },
+    {
+      "category": "Amelia",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question":
+          "Did Amelia reach 1m subscribers?,making her the 9th hololive member",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Amelia",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "Amelia birth date",
+      "correct_answer": "6 January",
+      "incorrect_answers": ["15 January", "6 June", "15 June"]
+    },
+  ]
+};
